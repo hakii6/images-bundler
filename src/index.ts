54274aa46a033adf0864dc44511b4a6f505b8fb1
unknown <ty5220669123@gmail.com>
2022-09-22 17:22:45 +0800

    base

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,66 @@
 // ref: https://github.com/Hopding/pdf-lib#embed-png-and-jpeg-images
 // import fetch from 'node-fetch';
-import { PDFDocument, rgb } from 'pdf-lib';
+import { PDFDocument, PDFImage } from 'pdf-lib';
 import fs from 'fs';
+import path from 'path';
 
 const rootDir = process.env.ROOT_DIR;
 
-const bookName = 'aaa';
+const books = fs.readdirSync(rootDir);
 
-const imagesUrl = [
-  './src/4.jpg',
-  // './src/1.jpg',
-  './src/2.jpg',
-  // './src/3.jpg',
-];
+for (const bookName of books) {
+  const bookPath = path.join(rootDir, bookName);
+  const bookPdfPath = bookPath + '.pdf';
+  if (
+    !fs.lstatSync(bookPath).isDirectory() ||
+    fs.existsSync(bookPdfPath)
+  ) {
+    continue;
+  }
 
-const pdfDoc = await PDFDocument.create();
+  const pdfDoc = await PDFDocument.create();
+  const imageNameArray = fs
+    .readdirSync(bookPath)
+    .sort((a, b) => {
+      return (
+        Number(a.slice(0, a.indexOf('.'))) -
+        Number(b.slice(0, b.indexOf('.')))
+      );
+    });
 
-for (const imageUrl of imagesUrl) {
-  const jpgImageBytes = fs.readFileSync(imageUrl);
-  const jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
-  const page = pdfDoc.addPage();
-  // scale to same width with pdf
-  const ratio = page.getWidth() / jpgImage.width;
-  const jpgDims = jpgImage.scale(ratio);
+  for (const imageName of imageNameArray) {
+    const imagePath = path.join(bookPath, imageName);
+    const imageBytes = fs.readFileSync(imagePath);
 
-  page.drawImage(jpgImage, {
-    // centeralize
-    x: page.getWidth() / 2 - jpgDims.width / 2,
-    y: page.getHeight() / 2 - jpgDims.height / 2,
-    width: jpgDims.width,
-    height: jpgDims.height,
-  });
+    let image: PDFImage;
+    switch (path.extname(imagePath)) {
+      case '.jpg':
+        image = await pdfDoc.embedJpg(imageBytes);
+        break;
+      case '.png':
+        image = await pdfDoc.embedPng(imageBytes);
+        break;
+      default:
+        image = await pdfDoc.embedJpg(imageBytes);
+        break;
+    }
+    const page = pdfDoc.addPage();
+    // scale to same width with pdf
+    const ratio = page.getWidth() / image.width;
+    const dims = image.scale(ratio);
+
+    page.drawImage(image, {
+      // centeralize
+      x: page.getWidth() / 2 - dims.width / 2,
+      y: page.getHeight() / 2 - dims.height / 2,
+      width: dims.width,
+      height: dims.height,
+    });
+  }
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save();
+
+  console.log(bookPdfPath);
+  fs.writeFileSync(bookPdfPath, pdfBytes);
 }
-
-// Serialize the PDFDocument to bytes (a Uint8Array)
-const pdfBytes = await pdfDoc.save();
-
-fs.writeFileSync(bookName + '.pdf', pdfBytes);
-
-// Trigger the browser to download the PDF document
-// download(pdfBytes, "pdf-lib_image_embedding_example.pdf", "application/pdf");
